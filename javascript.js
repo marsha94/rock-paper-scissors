84af3a698d6d5c912905e3ebc209e1f7d269d34f
MarshaMae <mae.marsha@gmail.com>
2023-12-23 14:18:47 +0800

    feat: Create PseudoCode & index.html

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// * Interface: No interface
+// * Input: Prompt user for choice (rock, paper, scissors)
+// * Output: Win, lose, draw. Depends on user and computer choice
+
+// Todo: Set playerSelection to get prompt from user (rock, paper, scissors)
+// Make sure selection is small letters before assigning
+// Todo: WHILE playerSelection is not correct, keep asking user for correct input. IF correct THEN break loop.
+// Todo: Set computerSelection to CALL getComputerChoice()
+// Todo: FUNCTION getComputerChoice (rock, paper, scissors) randomly
+// Todo: FUNCTION playRound comparing playerSelection & computerSelection
+/* 
+    IF playerSelection === computerSelection THEN
+      Set result = 'It's a Draw!'
+    ELSE IF (playerSelection = 'rock' AND computerSelection = 'scissors') OR (playerSelection = 'paper' AND computerSelection = 'rocks') OR (playerSelection = 'scissors' AND computerSelection = 'paper')
+      Set result = 'You Win! playerSelection beats computerSelection
+    ELSE 
+      Set result = 'You Lose! computerSelection beats playerSelection'
+    END IF
+    RETURN result
+*/
+// Todo: Display results
