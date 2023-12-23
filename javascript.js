ab5da39b41c1735380bd76cf87a9b1d1101f8c4a
MarshaMae <mae.marsha@gmail.com>
2023-12-23 15:52:13 +0800

    feat: Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,20 +2,43 @@
 // * Input: Prompt user for choice (rock, paper, scissors)
 // * Output: Win, lose, draw. Depends on user and computer choice
 
-// Todo: Set playerSelection to get prompt from user (rock, paper, scissors)
-// Make sure selection is small letters before assigning
-// Todo: WHILE playerSelection is not correct, keep asking user for correct input. IF correct THEN break loop.
-// Todo: Set computerSelection to CALL getComputerChoice()
-// Todo: FUNCTION getComputerChoice (rock, paper, scissors) randomly
-// Todo: FUNCTION playRound comparing playerSelection & computerSelection
-/* 
-    IF playerSelection === computerSelection THEN
-      Set result = 'It's a Draw!'
-    ELSE IF (playerSelection = 'rock' AND computerSelection = 'scissors') OR (playerSelection = 'paper' AND computerSelection = 'rocks') OR (playerSelection = 'scissors' AND computerSelection = 'paper')
-      Set result = 'You Win! playerSelection beats computerSelection
-    ELSE 
-      Set result = 'You Lose! computerSelection beats playerSelection'
-    END IF
-    RETURN result
-*/
-// Todo: Display results
+const choice = ["rock", "paper", "scissors"];
+let playerSelection = prompt(
+  "Incorrect input. Please chose a selection: rock, paper, scissors"
+).toLowerCase();
+
+while (!choice.includes(playerSelection)) {
+  playerSelection = prompt(
+    "Incorrect input. Please chose a selection: rock, paper, scissors"
+  ).toLowerCase();
+}
+
+let computerSelection = getComputerChoice();
+function getComputerChoice() {
+  choiceIndex = Math.floor(Math.random() * 3);
+  return choice[choiceIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  let result = "";
+  if (playerSelection === computerSelection) {
+    result = "It's a draw. ";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rocks") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    result = `You Win! ${
+      playerSelection[0].toUpperCase() + playerSelection.slice(1)
+    } beats ${
+      computerSelection[0].toUpperCase() + computerSelection.slice(1)
+    }.`;
+  } else {
+    result = `You Lose! ${
+      computerSelection[0].toUpperCase() + computerSelection.slice(1)
+    } beats ${playerSelection[0].toUpperCase() + playerSelection.slice(1)}.`;
+  }
+  return result;
+}
+
+console.log(playRound(playerSelection, computerSelection));
